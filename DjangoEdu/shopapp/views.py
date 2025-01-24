9bd9a782df18d468a8e3079a05df79273585b704
SirBay62 <sir.bay@yandex.ru>
2025-01-24 12:45:01 +0300

    Переопределение/дополнение шаблонов

--- DjangoEdu/shopapp/views.py
+++ DjangoEdu/shopapp/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render, HttpResponse
+from email.policy import default
+from timeit import default_timer
+
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
 
 
-def shop_index(request):
-    return HttpResponse('hellow world')
+def shop_index(request: HttpRequest):
+    context = {
+        "time_running":default_timer()
+    }
+    return render( request, "shopapp/shop-index.html", context = context )
