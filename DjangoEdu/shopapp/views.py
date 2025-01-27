7a522402598ae12e0b657b48d461979fcf17d5dc
SirBay62 <sir.bay@yandex.ru>
2025-01-27 14:38:05 +0300

    Добавлен вывод разрешений групп

--- DjangoEdu/shopapp/views.py
+++ DjangoEdu/shopapp/views.py
@@ -1,8 +1,9 @@
-from email.policy import default
+# from email.policy import default
 from timeit import default_timer
 
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from django.contrib.auth.models import Group
 
 
 def shop_index(request: HttpRequest):
@@ -16,3 +17,9 @@ def shop_index(request: HttpRequest):
         "products": products,
     }
     return render( request, "shopapp/shop-index.html", context = context )
+
+def groups_list(request: HttpRequest):
+    context = {
+        'groups': Group.objects.all(),
+    }
+    return render(request, template_name="shopapp/shop-groups.html", context = context )
