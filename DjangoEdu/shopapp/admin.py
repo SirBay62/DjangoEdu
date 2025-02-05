c0f58a42157f40547327d5b7a1f3168d19a14796
SirBay62 <sir.bay@yandex.ru>
2025-02-05 15:58:41 +0300

    Добавлено сокращенное описание

--- DjangoEdu/shopapp/admin.py
+++ DjangoEdu/shopapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+
+@admin.register ( Product )
+class ProductAdmin ( admin.ModelAdmin ):
+    # list_display = ('pk', 'name', 'description', 'price', 'discount')
+    list_display = ('pk', 'name', 'description_short', 'price', 'discount')
+    list_display_links = ('name', 'pk')
+
+    def description_short(self, obj: Product) -> str:
+        if obj.description is None:
+            return 'no description'
+        if len ( obj.description ) > 48:
+            return obj.description[:48] + '...'
+        return obj.description
+
+# admin.site.register ( Product, ProductAdmin )
--- DjangoEdu/shopapp/models.py
+++ DjangoEdu/shopapp/models.py
@@ -2,19 +2,33 @@ from django.contrib.auth.models import User
 
 from django.db import models
 
-class Product(models.Model):
+class Product ( models.Model ):
     class Meta:
         ordering = ('name',)
-    name = models.CharField(max_length=100)
-    description = models.TextField(null=True, blank=True)
-    price = models.DecimalField(default=0,max_digits=10, decimal_places=2)
-    discount = models.SmallIntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
-    archived = models.BooleanField(default=False)
 
-class Order(models.Model):
-    delivery_address = models.TextField(null=True, blank=True)
-    promocode = models.CharField(max_length=20, null=False, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
-    products = models.ManyToManyField(Product, related_name='orders')
+    name = models.CharField ( max_length=100 )
+    description = models.TextField ( null=True, blank=True )
+    price = models.DecimalField ( default=0, max_digits=10, decimal_places=2 )
+    discount = models.SmallIntegerField ( default=0 )
+    created_at = models.DateTimeField ( auto_now_add=True )
+    archived = models.BooleanField ( default=False )
+
+    # @property
+    # def description_short(self)->str:
+    #
+    #     if self.description is None:
+    #         return 'no description'
+    #     if len(self.description)<48:
+    #         return self.description
+    #     return self.description[:48] + '...'
+
+    def __str__(self) -> str:
+        return f"Product(pk={self.pk}, name={self.name!r})"
+
+class Order ( models.Model ):
+    delivery_address = models.TextField ( null=True, blank=True )
+    promocode = models.CharField ( max_length=20, null=False, blank=True )
+    created_at = models.DateTimeField ( auto_now_add=True )
+    user = models.ForeignKey ( User, on_delete=models.PROTECT )
+    products = models.ManyToManyField ( Product, related_name='orders' )
+
