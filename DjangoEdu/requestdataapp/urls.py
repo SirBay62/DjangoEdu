44fa3c1bcd6a0bba5eb9e923295783ee95a4fc60
SirBay62 <sir.bay@yandex.ru>
2025-02-11 13:58:53 +0300

    Добавлена загрузка файла из формы

--- DjangoEdu/requestdataapp/urls.py
+++ DjangoEdu/requestdataapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import process_get_view, user_form
+from .views import process_get_view, user_form, handle_file_upload
 
 app_name = 'requestdataapp'
 
@@ -8,6 +8,7 @@ urlpatterns = [
     # path('', process_get_view),
     path('get/', process_get_view),
     path('bio/', user_form, name='user-form'),
+    path('upload/', handle_file_upload, name='file-upload'),
 ]
 
 
