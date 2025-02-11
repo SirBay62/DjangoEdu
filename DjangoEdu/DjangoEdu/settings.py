767d233fd87dabfa4501c650fad506dab7d0a4c9
SirBay62 <sir.bay@yandex.ru>
2025-02-11 16:26:41 +0300

    midleware add

--- DjangoEdu/DjangoEdu/settings.py
+++ DjangoEdu/DjangoEdu/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'requestdataapp.middlewares.set_useragent_on_request_middleware',
+    'requestdataapp.middlewares.CountRequestMiddleware',
 ]
 
 ROOT_URLCONF = 'DjangoEdu.urls'
