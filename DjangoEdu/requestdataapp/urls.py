80ac8db5a2d0a7fc385c57e276fbbba893b8dd0c
SirBay62 <sir.bay@yandex.ru>
2025-02-11 12:40:46 +0300

    Post запрос добавлен

--- DjangoEdu/requestdataapp/urls.py
+++ DjangoEdu/requestdataapp/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from .views import process_get_view
+from .views import process_get_view, user_form
 
 app_name = 'requestdataapp'
 
 urlpatterns = [
     # path('', process_get_view),
     path('get/', process_get_view),
+    path('bio/', user_form, name='user-form'),
 ]
 
 
