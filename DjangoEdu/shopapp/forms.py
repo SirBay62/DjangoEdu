fac3f1d5ff3819224bf34158c68385aef8bc962f
SirBay62 <sir.bay@yandex.ru>
2025-02-13 14:08:03 +0300

    Формы на основе модели данных

--- DjangoEdu/shopapp/forms.py
+++ DjangoEdu/shopapp/forms.py
@@ -1,15 +1,22 @@
 from django import forms
 from django.core import validators
+from shopapp.models import Product
 
-class ProductForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    price = forms.DecimalField(min_value=1, max_value = 100000)
-    description = forms.CharField(
-        label = 'Product description',
-        widget=forms.Textarea(attrs={'rows':10, 'cols':50}),
-        validators=[validators.RegexValidator(
-           regex=r'great',
-           message="Product description must contain word 'great'."
-        )]
-    )
+
+# class ProductForm(forms.Form):
+#     name = forms.CharField(max_length=100)
+#     price = forms.DecimalField(min_value=1, max_value = 100000)
+#     description = forms.CharField(
+#         label = 'Product description',
+#         widget=forms.Textarea(attrs={'rows':10, 'cols':50}),
+#         validators=[validators.RegexValidator(
+#            regex=r'great',
+#            message="Product description must contain word 'great'."
+#         )]
+#     )
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields ='name', 'price', 'description', 'discount'
 
