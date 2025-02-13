7bad3fd2a91468a42040632cdb4cb207a1c877bb
SirBay62 <sir.bay@yandex.ru>
2025-02-13 10:39:02 +0300

    Валидаторы

--- DjangoEdu/shopapp/forms.py
+++ DjangoEdu/shopapp/forms.py
@@ -4,5 +4,12 @@ from django.core import validators
 class ProductForm(forms.Form):
     name = forms.CharField(max_length=100)
     price = forms.DecimalField(min_value=1, max_value = 100000)
-    description = forms.CharField(label = 'Product description',widget=forms.Textarea)
+    description = forms.CharField(
+        label = 'Product description',
+        widget=forms.Textarea(attrs={'rows':5}),
+        validators=[validators.RegexValidator(
+           regex=r'great',
+           message="Product description must contain word 'great'."
+        )]
+    )
 
