b0beecf885339db816e08adfe33bd88b648cfbba
SirBay62 <sir.bay@yandex.ru>
2025-03-25 16:25:39 +0300

    rss add

--- DjangoEdu/blogapp/urls.py
+++ DjangoEdu/blogapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import (
     ArticlesListView,
     ArticleDetailView,
+    LatestArticlesFeed
 )
 
 app_name = 'blogapp'
@@ -10,4 +11,5 @@ app_name = 'blogapp'
 urlpatterns = [
     path ( 'articles/', ArticlesListView.as_view (), name='articles' ),
     path ( 'article/<int:pk>/', ArticleDetailView.as_view (), name='article' ),
+    path('articles/latest/feed/', LatestArticlesFeed(), name='articles-feed' ),
 ]
